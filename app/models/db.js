da4e88d73ba8b2bf71d35a472a37701ffbab548f
lalitkt4 <lalitk@techfoursolutions.com>
2020-07-09 17:31:51 +0530

    remove junk code

--- app/models/db.js
+++ app/models/db.js
@@ -1,10 +1,6 @@
 const mysql = require("mysql");
-const dbConfig = require("../config/db.config.js");
+//const dbConfig = require("../config/db.config.js");
 const config = require('../config/default.json');
-
-//console.log(config.app.Mysql)
-
-//let checkMysql = config.app.Mysql;
 
 if ( config.app.Mysql === true ){
     // Create a connection to the database
